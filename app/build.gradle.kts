1fd129fa7127b0d27da6fec9672161050871dd42
Batuhan Öztürk <bozturk1999@gmail.com>
2024-04-26 23:37:43 +0300

    room library added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,11 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2024.04.01"))
+
+    //Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
