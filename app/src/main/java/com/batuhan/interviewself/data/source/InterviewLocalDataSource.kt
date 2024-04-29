83034d1b603ae50061f9e06242c03b56c89954e7
Batuhan Öztürk <bozturk1999@gmail.com>
2024-04-29 23:09:51 +0300

    gradle build missingbinding error is fixed, data source module removed
    datasource module not needed, removed

--- app/src/main/java/com/batuhan/interviewself/data/source/InterviewLocalDataSource.kt
+++ app/src/main/java/com/batuhan/interviewself/data/source/InterviewLocalDataSource.kt
@@ -3,21 +3,22 @@ package com.batuhan.interviewself.data.source
 import com.batuhan.interviewself.data.model.Interview
 import com.batuhan.interviewself.data.model.InterviewStep
 import com.batuhan.interviewself.db.InterviewDao
+import com.batuhan.interviewself.db.InterviewselfDatabase
 import javax.inject.Inject
 
-class InterviewLocalDataSource @Inject constructor(private val interviewDao: InterviewDao) {
+class InterviewLocalDataSource @Inject constructor(private val database: InterviewselfDatabase) {
 
-    fun getAllInterviews() = interviewDao.getAllInterviews()
+    fun getAllInterviews() = database.interviewDao.getAllInterviews()
 
-    suspend fun deleteInterview(interview: Interview) = interviewDao.deleteInterview(interview)
+    suspend fun deleteInterview(interview: Interview) = database.interviewDao.deleteInterview(interview)
 
-    suspend fun upsertInterview(interview: Interview) = interviewDao.upsertInterview(interview)
+    suspend fun upsertInterview(interview: Interview) = database.interviewDao.upsertInterview(interview)
 
-    suspend fun getInterviewWithSteps(interviewId: Long) = interviewDao.getInterviewWithSteps(interviewId)
+    suspend fun getInterviewWithSteps(interviewId: Long) = database.interviewDao.getInterviewWithSteps(interviewId)
 
-    suspend fun upsertInterviewStep(interviewStep: InterviewStep) = interviewDao.upsertInterviewStep(interviewStep)
+    suspend fun upsertInterviewStep(interviewStep: InterviewStep) = database.interviewDao.upsertInterviewStep(interviewStep)
 
-    suspend fun deleteInterviewStep(interviewStep: InterviewStep) = interviewDao.deleteInterviewStep(interviewStep)
+    suspend fun deleteInterviewStep(interviewStep: InterviewStep) = database.interviewDao.deleteInterviewStep(interviewStep)
 
-    suspend fun deleteInterviewSteps(id:Long) = interviewDao.deleteInterviewSteps(id)
+    suspend fun deleteInterviewSteps(id:Long) = database.interviewDao.deleteInterviewSteps(id)
 }
