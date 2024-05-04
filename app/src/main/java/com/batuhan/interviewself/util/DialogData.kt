10c17f01a44463ac6e4b03330f96f16f6c28d797
Batuhan Öztürk <bozturk1999@gmail.com>
2024-05-04 15:53:34 +0300

    browser event, dialogdata and settings detail action classes created

--- app/src/main/java/com/batuhan/interviewself/util/DialogData.kt
+++ app/src/main/java/com/batuhan/interviewself/util/DialogData.kt
@@ -2,11 +2,13 @@ package com.batuhan.interviewself.util
 
 import androidx.annotation.StringRes
 import androidx.compose.ui.graphics.Color
+import com.batuhan.interviewself.ui.theme.Gray
 
 data class DialogData(
     @StringRes val title: Int,
     val actions: List<DialogAction>,
-    val type: DialogType = DialogType.ERROR
+    val options: List<DialogAction>? = null,
+    val type: DialogType = DialogType.ERROR,
 )
 
 data class DialogAction(
@@ -15,5 +17,16 @@ data class DialogAction(
 )
 
 enum class DialogType(val containerColor: Color, val textColor: Color = Color.White) {
-    ERROR(Color.Red), SUCCESS_INFO(Color.Green), INFO(Color.White, Color.Black)
+    ERROR(Color(0xFFA0153E)),
+    SUCCESS_INFO(Color(0xFF4F6F52)),
+    DIALOG_LIGHT(
+        Color.White,
+        Gray,
+    ),
+    DIALOG_DARK(
+        Gray,
+        Color.White,
+    )
+
 }
+fun decideDialogType(darkMode: Boolean) = if(darkMode) DialogType.DIALOG_DARK else DialogType.DIALOG_LIGHT
