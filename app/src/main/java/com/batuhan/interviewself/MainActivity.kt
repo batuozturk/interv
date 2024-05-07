3bbb6e37f4f7b1699a1f7aea2d85431aea253d3d
Batuhan Öztürk <bozturk1999@gmail.com>
2024-05-07 21:21:34 +0300

    mainactivity runtime permissions added, interview screen implemented

--- app/src/main/java/com/batuhan/interviewself/MainActivity.kt
+++ app/src/main/java/com/batuhan/interviewself/MainActivity.kt
@@ -1,19 +1,19 @@
 package com.batuhan.interviewself
 
-import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.Manifest
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -21,10 +21,12 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import androidx.navigation.navOptions
 import com.batuhan.interviewself.MainActivity.Companion.KEY_INTERVIEW_ID
+import com.batuhan.interviewself.MainActivity.Companion.KEY_INTERVIEW_TYPE
 import com.batuhan.interviewself.presentation.container.ContainerScreen
 import com.batuhan.interviewself.presentation.interview.create.CreateInterviewScreen
 import com.batuhan.interviewself.presentation.interview.create.addstep.AddStepScreen
 import com.batuhan.interviewself.presentation.interview.detail.InterviewDetailScreen
+import com.batuhan.interviewself.presentation.interview.enter.InterviewScreen
 import com.batuhan.interviewself.presentation.splash.SplashScreen
 import com.batuhan.interviewself.ui.theme.InterviewselfTheme
 import com.batuhan.interviewself.util.BrowserEvent
@@ -35,15 +37,21 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     companion object {
         internal const val KEY_INTERVIEW_ID = "interview_id"
+        internal const val KEY_INTERVIEW_TYPE = "interview_type"
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
+            ).toTypedArray()
     }
 
     private lateinit var customTabsIntent: CustomTabsIntent
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
+        activityResultLauncher.launch(REQUIRED_PERMISSIONS)
         if (isTablet()) {
             requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         } else {
@@ -59,21 +67,37 @@ class MainActivity : ComponentActivity() {
                     InterviewSelfApp(
                         finishApplication = { this.finish() },
                         sendBrowserEvent = {
-                            customTabsIntent = CustomTabsIntent.Builder().setColorScheme(
-                                CustomTabsIntent.COLOR_SCHEME_SYSTEM
-                            ).setBookmarksButtonEnabled(true).build()
+                            customTabsIntent =
+                                CustomTabsIntent.Builder().setColorScheme(
+                                    CustomTabsIntent.COLOR_SCHEME_SYSTEM,
+                                ).setBookmarksButtonEnabled(true).build()
                             customTabsIntent.launchUrl(this, Uri.parse(it.url))
-                        }
+                        },
                     )
                 }
             }
         }
     }
+
+    private val activityResultLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions())
+        { permissions ->
+            // Handle Permission granted/rejected
+            var permissionGranted = true
+            permissions.entries.forEach {
+                if (it.value == false)
+                    permissionGranted = false
+            }
+            // todo
+        }
 }
 
 @Composable
-fun InterviewSelfApp(finishApplication: () -> Unit, sendBrowserEvent: (BrowserEvent) -> Unit) {
-
+fun InterviewSelfApp(
+    finishApplication: () -> Unit,
+    sendBrowserEvent: (BrowserEvent) -> Unit,
+) {
     // todo navigation keys and argument keys
     val navController = rememberNavController()
 
@@ -101,8 +125,8 @@ fun InterviewSelfApp(finishApplication: () -> Unit, sendBrowserEvent: (BrowserEv
                 navigateToDetail = {
                     navController.navigate("interview_detail/$it")
                 },
-                enterInterview = {
-                    navController.navigate("enter_interview/$it")
+                enterInterview = { id, type ->
+                    navController.navigate("enter_interview/$id/${type.name}")
                 },
                 sendBrowserEvent = sendBrowserEvent,
             )
@@ -143,23 +167,32 @@ fun InterviewSelfApp(finishApplication: () -> Unit, sendBrowserEvent: (BrowserEv
             InterviewDetailScreen(
                 interviewId = it.arguments?.getLong(KEY_INTERVIEW_ID) ?: -1,
                 onBackPressed = { navController.popBackStack() },
-                enterInterview = {
+                enterInterview = { id, type ->
                     navController.popBackStack()
-                    navController.navigate("enter_interview/$it")
-                }
+                    navController.navigate("enter_interview/$id/${type.name}")
+                },
             )
         }
 
         composable(
-            "enter_interview/{$KEY_INTERVIEW_ID}",
+            "enter_interview/{$KEY_INTERVIEW_ID}/{$KEY_INTERVIEW_TYPE}",
             arguments =
-            listOf(
-                navArgument(KEY_INTERVIEW_ID) {
-                    type = NavType.LongType
-                },
-            ),
-        ){
-
+                listOf(
+                    navArgument(KEY_INTERVIEW_ID) {
+                        type = NavType.LongType
+                    },
+                    navArgument(KEY_INTERVIEW_TYPE) {
+                        type = NavType.StringType
+                    },
+                ),
+        ) {
+            InterviewScreen(
+                interviewId = it.arguments?.getLong(KEY_INTERVIEW_ID) ?: -1,
+                interviewType = it.arguments?.getString(KEY_INTERVIEW_TYPE) ?: "",
+                onBackPressed = {
+                    navController.popBackStack()
+                }
+            )
         }
     }
 }
