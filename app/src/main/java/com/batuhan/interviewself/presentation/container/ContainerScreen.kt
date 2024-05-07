25e585cd062e3c672c852b386e6b78878a945dfe
Batuhan Öztürk <bozturk1999@gmail.com>
2024-05-07 21:20:56 +0300

    container screen enter interview dialog data update, enter interview logic update

--- app/src/main/java/com/batuhan/interviewself/presentation/container/ContainerScreen.kt
+++ app/src/main/java/com/batuhan/interviewself/presentation/container/ContainerScreen.kt
@@ -1,6 +1,5 @@
 package com.batuhan.interviewself.presentation.container
 
-import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -8,14 +7,12 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material.icons.sharp.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -23,10 +20,10 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import com.batuhan.interviewself.R
+import com.batuhan.interviewself.data.model.InterviewType
 import com.batuhan.interviewself.presentation.interview.InterviewListScreen
 import com.batuhan.interviewself.presentation.question.QuestionListScreen
 import com.batuhan.interviewself.presentation.settings.SettingsScreen
@@ -34,15 +31,16 @@ import com.batuhan.interviewself.ui.theme.fontFamily
 import com.batuhan.interviewself.util.BaseView
 import com.batuhan.interviewself.util.BrowserEvent
 import com.batuhan.interviewself.util.DialogData
-import com.batuhan.interviewself.util.isTablet
+import com.batuhan.interviewself.util.EnterInterviewDialogData
+import com.batuhan.interviewself.util.EnterInterviewView
 import kotlinx.coroutines.launch
 
 @Composable
 fun ContainerScreen(
     createInterview: () -> Unit,
     navigateToDetail: (Long) -> Unit,
-    enterInterview: (Long) -> Unit,
-    sendBrowserEvent: (BrowserEvent) -> Unit
+    enterInterview: (Long, InterviewType) -> Unit,
+    sendBrowserEvent: (BrowserEvent) -> Unit,
 ) {
     ContainerScreenContent(createInterview, navigateToDetail, enterInterview, sendBrowserEvent)
 }
@@ -52,8 +50,8 @@ fun ContainerScreen(
 fun ContainerScreenContent(
     createInterview: () -> Unit,
     navigateToDetail: (Long) -> Unit,
-    enterInterview: (Long) -> Unit,
-    sendBrowserEvent: (BrowserEvent) -> Unit
+    enterInterview: (Long, InterviewType) -> Unit,
+    sendBrowserEvent: (BrowserEvent) -> Unit,
 ) {
     val pagerState =
         rememberPagerState {
@@ -62,115 +60,137 @@ fun ContainerScreenContent(
     var dialogData: DialogData? by remember {
         mutableStateOf(null)
     }
+    var enterInterviewDialogData: EnterInterviewDialogData? by remember {
+        mutableStateOf(null)
+    }
 
     val coroutineScope = rememberCoroutineScope()
-    BaseView(dialogData) {
-        Scaffold(
-            modifier = Modifier.fillMaxSize(),
-            bottomBar = {
-                NavigationBar(containerColor = Color.Transparent) {
-                    NavigationBarItem(
-                        alwaysShowLabel = false,
-                        selected = pagerState.currentPage == 0,
-                        onClick = {
-                            dialogData = null
-                            coroutineScope.launch {
-                                pagerState.scrollToPage(0)
-                            }
-                        },
-                        icon = {
-                            Icon(
-                                painterResource(id = R.drawable.ic_quiz),
-                                contentDescription = null,
-                            )
-                        },
-                        label = {
-                            Text(stringResource(id = R.string.navitem_one), fontFamily = fontFamily)
-                        },
-                    )
-                    NavigationBarItem(
-                        alwaysShowLabel = false,
-                        selected = pagerState.currentPage == 1,
-                        onClick = {
-                            dialogData = null
-                            coroutineScope.launch {
-                                pagerState.scrollToPage(1)
-                            }
-                        },
-                        icon = {
-                            Icon(
-                                painterResource(id = R.drawable.ic_question_mark_24),
-                                contentDescription = null,
-                            )
-                        },
-                        label = {
-                            Text(stringResource(id = R.string.navitem_two), fontFamily = fontFamily)
-                        },
-                    )
-                    NavigationBarItem(
-                        alwaysShowLabel = false,
-                        selected = pagerState.currentPage == 2,
-                        onClick = {
-                            dialogData = null
-                            coroutineScope.launch {
-                                pagerState.scrollToPage(2)
-                            }
-                        },
-                        icon = {
-                            Icon(
-                                Icons.Outlined.Settings,
-                                contentDescription = null,
-                            )
-                        },
-                        label = {
-                            Text(
-                                stringResource(id = R.string.navitem_three),
-                                fontFamily = fontFamily,
-                            )
-                        },
-                    )
-                }
-            },
-        ) {
-            HorizontalPager(
-                state = pagerState,
-                modifier = Modifier.padding(it),
-                userScrollEnabled = false,
-            ) { page ->
-                when (page) {
-                    0 -> {
-                        InterviewListScreen(
-                            showDialog = { data ->
-                                dialogData = data
+    EnterInterviewView(data = enterInterviewDialogData) {
+        BaseView(dialogData) {
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                bottomBar = {
+                    NavigationBar(containerColor = Color.Transparent) {
+                        NavigationBarItem(
+                            alwaysShowLabel = false,
+                            selected = pagerState.currentPage == 0,
+                            onClick = {
+                                dialogData = null
+                                coroutineScope.launch {
+                                    pagerState.scrollToPage(0)
+                                }
                             },
-                            clearDialog = { dialogData = null },
-                            createInterview = createInterview,
-                            navigateToDetail = navigateToDetail,
-                            enterInterview = enterInterview,
+                            icon = {
+                                Icon(
+                                    painterResource(id = R.drawable.ic_quiz),
+                                    contentDescription = null,
+                                )
+                            },
+                            label = {
+                                Text(
+                                    stringResource(id = R.string.navitem_one),
+                                    fontFamily = fontFamily,
+                                )
+                            },
+                        )
+                        NavigationBarItem(
+                            alwaysShowLabel = false,
+                            selected = pagerState.currentPage == 1,
+                            onClick = {
+                                dialogData = null
+                                coroutineScope.launch {
+                                    pagerState.scrollToPage(1)
+                                }
+                            },
+                            icon = {
+                                Icon(
+                                    painterResource(id = R.drawable.ic_question_mark_24),
+                                    contentDescription = null,
+                                )
+                            },
+                            label = {
+                                Text(
+                                    stringResource(id = R.string.navitem_two),
+                                    fontFamily = fontFamily,
+                                )
+                            },
+                        )
+                        NavigationBarItem(
+                            alwaysShowLabel = false,
+                            selected = pagerState.currentPage == 2,
+                            onClick = {
+                                dialogData = null
+                                coroutineScope.launch {
+                                    pagerState.scrollToPage(2)
+                                }
+                            },
+                            icon = {
+                                Icon(
+                                    Icons.Outlined.Settings,
+                                    contentDescription = null,
+                                )
+                            },
+                            label = {
+                                Text(
+                                    stringResource(id = R.string.navitem_three),
+                                    fontFamily = fontFamily,
+                                )
+                            },
                         )
                     }
+                },
+            ) {
+                HorizontalPager(
+                    state = pagerState,
+                    modifier = Modifier.padding(it),
+                    userScrollEnabled = false,
+                ) { page ->
+                    when (page) {
+                        0 -> {
+                            InterviewListScreen(
+                                showDialog = { data ->
+                                    dialogData = data
+                                },
+                                clearDialog = { dialogData = null },
+                                createInterview = createInterview,
+                                navigateToDetail = navigateToDetail,
+                                enterInterview = { id, type ->
+                                    enterInterviewDialogData =
+                                        EnterInterviewDialogData(
+                                            id,
+                                            {
+                                                enterInterview.invoke(id, type)
+                                            },
+                                            {
+                                                enterInterviewDialogData = null
+                                            },
+                                        )
+                                },
+                            )
+                        }
 
-                    1 -> {
-                        QuestionListScreen(
-                            showDialog = { data ->
-                                dialogData = data
-                            },
-                            clearDialog = { dialogData = null },
-                        )
-                    }
+                        1 -> {
+                            QuestionListScreen(
+                                showDialog = { data ->
+                                    dialogData = data
+                                },
+                                clearDialog = { dialogData = null },
+                            )
+                        }
 
-                    2 -> {
-                        SettingsScreen(
-                            exportQuestions = { },
-                            importQuestions = { },
-                            language = { },
-                            setStyle = {},
-                            showDialog = { data ->
-                                dialogData = data
-                            },
-                            clearDialog = { dialogData = null },
-                            sendBrowserEvent = sendBrowserEvent
-                        )
+                        2 -> {
+                            SettingsScreen(
+                                exportQuestions = { },
+                                importQuestions = { },
+                                language = { },
+                                setStyle = {},
+                                showDialog = { data ->
+                                    dialogData = data
+                                },
+                                clearDialog = { dialogData = null },
+                                sendBrowserEvent = sendBrowserEvent,
+                            )
+                        }
                     }
                 }
             }
