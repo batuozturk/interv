da948962c444ecce915603c68ab1e42d64591edc
Batuhan Öztürk <bozturk1999@gmail.com>
2024-05-10 22:24:43 +0300

    usecases filter updates

--- app/src/main/java/com/batuhan/interviewself/domain/interview/GetAllInterviews.kt
+++ app/src/main/java/com/batuhan/interviewself/domain/interview/GetAllInterviews.kt
@@ -1,9 +1,12 @@
 package com.batuhan.interviewself.domain.interview
 
+import com.batuhan.interviewself.data.model.InterviewFilterType
 import com.batuhan.interviewself.data.repository.InterviewRepository
 import javax.inject.Inject
 
 class GetAllInterviews @Inject constructor(private val repository: InterviewRepository) {
 
-    operator fun invoke() = repository.getAllInterviews()
+    data class Params(val searchText: String, val filterType: InterviewFilterType)
+
+    operator fun invoke(params: Params) = repository.getAllInterviews(params.searchText, params.filterType)
 }
