6a4ba26b34d8d8c2361622251c54992fc141f902
Batuhan Öztürk <bozturk1999@gmail.com>
2024-05-10 22:23:54 +0300

    localdatasource filter updates

--- app/src/main/java/com/batuhan/interviewself/data/source/InterviewLocalDataSource.kt
+++ app/src/main/java/com/batuhan/interviewself/data/source/InterviewLocalDataSource.kt
@@ -1,14 +1,24 @@
 package com.batuhan.interviewself.data.source
 
+import androidx.sqlite.db.SimpleSQLiteQuery
 import com.batuhan.interviewself.data.model.Interview
+import com.batuhan.interviewself.data.model.InterviewFilterType
 import com.batuhan.interviewself.data.model.InterviewStep
-import com.batuhan.interviewself.db.InterviewDao
+import com.batuhan.interviewself.data.model.createQuery
 import com.batuhan.interviewself.db.InterviewselfDatabase
 import javax.inject.Inject
 
 class InterviewLocalDataSource @Inject constructor(private val database: InterviewselfDatabase) {
-
-    fun getAllInterviews() = database.interviewDao.getAllInterviews()
+    fun getAllInterviews(
+        searchText: String,
+        filterType: InterviewFilterType,
+    ) = database.interviewDao.getAllInterviews(
+        SimpleSQLiteQuery(
+            filterType.createQuery(searchText),
+            searchText.takeIf { it.isNotBlank() && it.isNotEmpty() }
+                ?.let { arrayOf(it + "%") },
+        ),
+    )
 
     suspend fun deleteInterview(interview: Interview) = database.interviewDao.deleteInterview(interview)
 
--- app/src/main/java/com/batuhan/interviewself/data/source/QuestionLocalDataSource.kt
+++ app/src/main/java/com/batuhan/interviewself/data/source/QuestionLocalDataSource.kt
@@ -1,13 +1,24 @@
 package com.batuhan.interviewself.data.source
 
+import androidx.sqlite.db.SimpleSQLiteQuery
 import com.batuhan.interviewself.data.model.Question
+import com.batuhan.interviewself.data.model.QuestionFilterType
+import com.batuhan.interviewself.data.model.createQuery
 import com.batuhan.interviewself.db.InterviewselfDatabase
-import com.batuhan.interviewself.db.QuestionDao
 import javax.inject.Inject
 
 class QuestionLocalDataSource @Inject constructor(private val database: InterviewselfDatabase) {
 
-    fun getAllQuestions() = database.questionDao.getAllQuestions()
+    fun getAllQuestions(
+        searchText: String,
+        filterType: QuestionFilterType,
+    ) = database.questionDao.getAllQuestions(
+        SimpleSQLiteQuery(
+            filterType.createQuery(searchText),
+            searchText.takeIf { it.isNotBlank() && it.isNotEmpty() }
+                ?.let { arrayOf(it + "%") },
+        )
+    )
 
     suspend fun deleteQuestion(question: Question) = database.questionDao.deleteQuestion(question)
 
