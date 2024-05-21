e3163eb693c0b7ffa8f95374e3c23cba19861321
Batuhan Öztürk <bozturk1999@gmail.com>
2024-05-22 01:09:07 +0300

    splash screen logo update

--- app/src/main/java/com/batuhan/interviewself/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/batuhan/interviewself/presentation/splash/SplashScreen.kt
@@ -2,8 +2,13 @@ package com.batuhan.interviewself.presentation.splash
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,9 +19,11 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.batuhan.interviewself.R
 import kotlinx.coroutines.delay
 
 @Composable
@@ -32,11 +39,14 @@ fun SplashScreen(
 
 @Composable
 fun SplashScreenContent(navigateMainScreen: () -> Unit) {
-    Box(
+    Row(
         modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background),
-        contentAlignment = Alignment.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
     ) {
-        AnimatedText(baseText = "interviewself", navigateMainScreen)
+        Icon(painterResource(id = R.drawable.ic_logo), contentDescription = null, modifier = Modifier.size(42.dp))
+        Spacer(Modifier.width(8.dp))
+        AnimatedText(baseText = "interv", navigateMainScreen)
     }
 }
 
