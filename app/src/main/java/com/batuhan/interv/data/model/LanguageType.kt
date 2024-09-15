d275fa7196eb2f0822c4fa6136ab6c2dc9c26632
Batuhan Öztürk <bozturk1999@gmail.com>
2024-09-16 01:11:29 +0300

    languagetypede added

--- app/src/main/java/com/batuhan/interv/data/model/LanguageType.kt
+++ app/src/main/java/com/batuhan/interv/data/model/LanguageType.kt
@@ -15,6 +15,6 @@ enum class LanguageType(
         "tr-TR",
     ),
     FR(R.string.language_type_fr, "fr-FR"),
-    DE(R.string.settings_lang_option_four, "de-DE"),
+    DE(R.string.language_type_de, "de-DE"),
     ES(R.string.language_type_es, "es-ES")
 }
