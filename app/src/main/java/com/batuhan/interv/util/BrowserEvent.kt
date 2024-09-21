97a467bf2f4b2f2d47019838d0cf9888b161b303
Batuhan Öztürk <bozturk1999@gmail.com>
2024-09-21 22:05:11 +0300

    browser event update app implementation

--- app/src/main/java/com/batuhan/interv/util/BrowserEvent.kt
+++ app/src/main/java/com/batuhan/interv/util/BrowserEvent.kt
@@ -6,4 +6,5 @@ sealed class BrowserEvent(val url: String) {
     object ContactUs: BrowserEvent("mailto:batuoztrk99@gmail.com")
     object DownloadKonsol: BrowserEvent("https://play.google.com/store/apps/details?id=com.batuhan.konsol")
     object PrivacyPolicy: BrowserEvent("https://sites.google.com/view/interv-privacy-policy/")
+    object UpdateApp: BrowserEvent("https://play.google.com/store/apps/details?id=com.batuhan.interv")
 }
