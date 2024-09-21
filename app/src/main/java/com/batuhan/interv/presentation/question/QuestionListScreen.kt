670ad50e0eb065757e7cce3c627041976471ed05
Batuhan Öztürk <bozturk1999@gmail.com>
2024-09-21 15:03:22 +0300

    questionlistemptyview gridcell span update for tablets

--- app/src/main/java/com/batuhan/interv/presentation/question/QuestionListScreen.kt
+++ app/src/main/java/com/batuhan/interv/presentation/question/QuestionListScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -223,7 +224,7 @@ fun QuestionListScreenContentForTablet(
                     }
                 }
                 if (loadState.source.refresh is LoadState.NotLoading && loadState.append.endOfPaginationReached && questions.itemCount == 0) {
-                    item {
+                    item(span = { GridItemSpan(2) }) {
                         EmptyQuestionView()
                     }
                 }
